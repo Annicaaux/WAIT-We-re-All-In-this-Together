5a32b831aa585f99b8a1aa0fe1877faf3db03966
Annicaaux <klinke.annica@gmail.com>
2025-06-27 20:10:38 +0200

    Update wait.py

--- wait.py
+++ wait.py
@@ -728,5 +728,6 @@ def main():
         
            st.markdown(f"<div class='postit postit-secondary'>{text}</div>", unsafe_allow_html=True)
            
-
+if __name__ == "__main__":
+    main()
                 
