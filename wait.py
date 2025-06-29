bf57913c6980c8cdd280985d5150b0e222dc56a0
Annicaaux <klinke.annica@gmail.com>
2025-06-29 22:59:12 +0200

    Update wait.py

--- wait.py
+++ wait.py
@@ -215,8 +215,8 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs([
 ])
 
 with tab1:
-    st.header
-    <h1 style="color: #8b3a3a; ">🌿 Gesunde Pausen für Körper & Geist"</h1>
+    st.header ("Gesunde Pausen für Körper & Geist")
+    <h1 style="color: #8b3a3a; ">Gesunde Pausen für Körper & Geist"</h1>
    
     
     st.markdown("""
