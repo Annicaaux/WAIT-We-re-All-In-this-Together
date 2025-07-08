d0e04696b99b05a0ebee49515036d0855ff93a5d
Annicaaux <klinke.annica@gmail.com>
2025-07-09 00:55:13 +0200

    Update wait.py

--- wait.py
+++ wait.py
@@ -1598,7 +1598,7 @@ with tab7:
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         # Spotify Song URL - KORRIGIERT
-        spotify_song_url = f"https://open.spotify.com/playlist/062JO6PmxDjuL3MfYZtA2h?si=6418baa8c6884994&pt=1f8b6894e436577897f00501a6627672/{song_info['spotify_id']}"
+        spotify_song_url = f"https://open.spotify.com/playlist/062JO6PmxDjuL3MfYZtA2h/{song_info['spotify_id']}"
         
         # Großer Spotify-Button mit HTML/CSS - KORRIGIERT
         st.markdown(f"""
